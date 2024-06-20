36ab79e32615f1e199d16468db6591ff80e64052
Gospel Amanze <ollygos@gmail.com>
2024-06-20 17:27:37 +0100

    refactor(auth): update eslint and prettier configs

--- auth/src/routes/__tests__/signup.test.ts
+++ auth/src/routes/__tests__/signup.test.ts
@@ -8,5 +8,8 @@ import app from '../../app';
 it('should return 422 if the email is not valid', async () => {
   await request(app).post('/api/auth/signup').send({}).expect(422);
 
-  await request(app).post('/api/auth/signup').send({ email: 'invalidEmail' }).expect(422);
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'invalidEmail' })
+    .expect(422);
 });
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,19 +1,22 @@
-import { defaultMaxListeners } from 'events';
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
 const signupRouter = express.Router();
+
+const test = `test`;
 
 signupRouter.post(
   '/api/auth/signup',
   [body('email').isEmail().withMessage('Email must be in a valid format')],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       res.status(422).send({});
     }
+
     res.send({});
-  }
+  },
 );
 
 export default signupRouter;
